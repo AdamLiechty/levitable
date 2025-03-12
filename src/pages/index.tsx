e4f24b29581c78c8243e228527538d86d23f8b71
Adam Liechty <aliechty@salesforce.com>
2025-03-12 01:27:25 -0700

    video style fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,9 @@ export default function Home(): JSX.Element {
 
         <div className="text--center">
           <div className="video-container">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/dY9e-6SCJXY?si=6o3fmudmRNCU_dUC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/dY9e-6SCJXY?si=6o3fmudmRNCU_dUC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            </div>
           </div>
           <br/><br/>
           <AppBadge /> <br/><br/>
